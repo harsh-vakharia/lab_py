06a38b54bf418de1671e19076e71ee5b6aa92a51
harsh-vakharia <harshvakharia7@gmail.com>
2026-01-05 11:31:38 +0530

    Add files via upload

---
+++ lab exercise.py
@@ -0,0 +1,29 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+x="hello world"
+print(x)
+hello world
+x=50
+print(x)
+50
+x='true'
+print(x)
+true
+x=("apple","banana","cherry")
+print(x)
+('apple', 'banana', 'cherry')
+x=["apple","banana","cherry"]
+print(x)
+['apple', 'banana', 'cherry']
+x=range(9)
+print (list (x))
+[0, 1, 2, 3, 4, 5, 6, 7, 8]
+x={"name":"harsh","age":21}
+print(x)
+{'name': 'harsh', 'age': 21}
+x=3j
+print(x)
+3j
+x=40.5
+print(x)
+40.5
