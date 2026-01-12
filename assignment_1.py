586bdbd8e2c52c693b1ba56ee1e4309a74f66bc0
harsh-vakharia <harshvakharia7@gmail.com>
2026-01-12 11:34:37 +0530

    Add files via upload

---
+++ assignment_1.py
@@ -0,0 +1,44 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+x="welcome message"
+print(x)
+welcome message
+x=("Rajesh Kumar\n\t Flat No.101,Sunshine Apartments\n\t MG Road,Sector 360004\n\t India")
+print(x)
+Rajesh Kumar
+	 Flat No.101,Sunshine Apartments
+	 MG Road,Sector 360004
+	 India
+x=150
+y=120.50
+print("Addition:",x+y)
+Addition: 270.5
+print("subtraction:"x-y)
+SyntaxError: invalid syntax. Perhaps you forgot a comma?
+print("subtraction:",x-y)
+subtraction: 29.5
+print("multiplication",x*y)
+multiplication 18075.0
+print("division:",x/y)
+division: 1.2448132780082988
+x="Area of a circle=3.14*radius2"
+print(x)
+Area of a circle=3.14*radius2
+x="circumference of a circle=2*pi*radius"
+print(x)
+circumference of a circle=2*pi*radius
+x="Simple Interest=(P*R*T)/100"
+print(x)
+Simple Interest=(P*R*T)/100
+x="Perimeter Of Reactangle=2*(Length+Width)"
+print(x)
+Perimeter Of Reactangle=2*(Length+Width)
+x="Perimeter Of Traiangle=A+B+C"
+print(x)
+Perimeter Of Traiangle=A+B+C
+x="Area Of Square=Side*Side"
+print(x)
+Area Of Square=Side*Side
+x="Perimeter Of Square=4*Side"
+print(x)
+Perimeter Of Square=4*Side
