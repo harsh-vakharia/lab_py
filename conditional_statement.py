fb1f9d7f27c33edef2a69da8433d8f77ee2340fa
harsh-vakharia <harshvakharia7@gmail.com>
2026-02-09 11:05:47 +0530

    Add files via upload

---
+++ conditional_statement.py
@@ -0,0 +1,14 @@
+""" age = int(input("enter your age:"))
+if age >= 18:
+    print("yes you are eligible")
+else:
+    print("you arer not eligible")"""
+    
+    
+marks = int(input("enter your marks:"))
+if marks >= 90:
+    print("you will get a greade")
+elif marks >=70:
+    print("you will get b greade")
+else:
+    print("you will get c greade")
